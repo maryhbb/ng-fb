3e8604835814e78587881920faeb02ec40bfabed
Mary <mary_hbb@yahoo.com>
2023-02-02 00:45:18 +0100

    Format

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -5,27 +5,20 @@ import { DataserviceService } from 'src/app/servieces/dataservice.service';
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
-  styleUrls: ['./post.component.css']
+  styleUrls: ['./post.component.css'],
 })
 export class PostComponent {
- 
+  constructor(private dataService: DataserviceService) {}
 
-  constructor(private dataService: DataserviceService) { }
-
-
-  postForm: FormGroup= new FormGroup({
+  postForm: FormGroup = new FormGroup({
     username: new FormControl(''),
     date: new FormControl(''),
-    message: new FormControl('')
-
-
-    
-    });
- 
+    message: new FormControl(''),
+  });
 
   shareIt(): void {
     console.log(this.postForm.value);
     this.dataService.posts.push(this.postForm.value);
-    console.log(this.dataService.posts)
+    console.log(this.dataService.posts);
   }
 }
